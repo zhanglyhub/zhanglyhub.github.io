9e4f1c8146c97dc4de380e22b74bd3982c974a01
zhanglyhub <theshine_king@outlook.com>
2025-06-15 10:06:51 -0700

    Site updated: 2025-06-15 10:06:46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749912217709";const preCache = ["/images/taichi.png","/crossorigin=%22anonymous%22%20-%20%22/images/a.webp%22%20-%20%22/images/b.webp%22%20-%20%22/images/c.webp%22","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1750007132329";const preCache = ["/images/taichi.png","/crossorigin=%22anonymous%22%20-%20%22/images/a.webp%22%20-%20%22/images/b.webp%22%20-%20%22/images/c.webp%22","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
